c95048a6dc1d472bcb6d3c6b8f11a2b133c215c6
Deep Patel <deepgpatel93@gmail.com>
2018-01-29 23:49:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ function logWhisper(string)
 
 function sayHiToGrandma(string)
 {
-  if (string.toLowerCase() === 'hello') {
+  if (string.toLowerCase() === string) {
     return "I can\'t hear you!"
-  } else if (string.toUpperCase() === 'HELLO') {
+  } else if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
   
